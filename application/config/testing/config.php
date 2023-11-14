ab96668293d1a8b912fd909e9c56a71d2f9c861e
BAKARY AINENY SANE <bakary.sane@education.sn>
2023-11-14 16:19:15 +0000

    Suite

--- application/config/testing/config.php
+++ application/config/testing/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/simen/';
+$config['base_url'] = 'http://localhost:81/sunuparc2/';
 
 $config['index_page'] = '';
 
